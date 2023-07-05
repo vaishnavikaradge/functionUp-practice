cb39ddbb34f5823a221cb1cf1b953ba690b0586f
vaishnavikaradge <vaishnavikaradge@gmail.com>
2023-07-05 16:33:36 +0530

    Form Validation for Student

---
+++ formvalidation/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FormValidation from './component/FormValidation';
+
+function App() {
+  return (
+    <div className="App">
+      <FormValidation/>
+    </div>
+  );
+}
+
+export default App;
